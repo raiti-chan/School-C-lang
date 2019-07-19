09d75ed455472066803b8d2c163019889789cd69
雷知 結 <rusuke@t-net.ne.jp>
2019-07-19 10:45:42 +0900

    fix code kadai0310

--- 003/kadai/kadai0310.c
+++ 003/kadai/kadai0310.c
@@ -15,7 +15,7 @@ void main() {
 		printf("再トライすべし");
 	} else if (59 >= val) {
 		printf("努力を要す");
-	} else if (79 > val){
+	} else if (79 >= val){
 		printf("合格");
 	} else if (99 > val) {
 		printf("期待以上");
